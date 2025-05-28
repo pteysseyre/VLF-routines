52d7991e15ac79c6e496716f3f244754d173e828
pteysseyre <pauline.teysseyre@obspm.fr>
2025-05-28 11:18:25 +0200

    Added read_Narrowband and  doc

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # VLF-routines
 This repository contains short functions that can be used to do basic operations on VLF data
+
+
+# Example
+
+## Reading files
+
+To read Narrowband, the `read_Narrowband` function can be called. To do so, you need:
+
+1. The date, in the correct format (e.g. '2023_11_05')
+2. The call-sign of the function (e.g. 'GVT')
+3. The path to the files. It is assumed that the files are organised in a file folder 
+    and in subfolders named `year/month/date/`. If this is not the case, it can be changed 
+    in the fonction (see the relevant line, indicated at the start of the function)
+4. (Optional) The calibration factors for the station and each orientation
+
+> [!NOTE]
+> It is assumed that the files being read have the same naming convention as the ones from the AWESOME antennas. 
+> In particular, their names should end my `100A.mat`, `101A.mat`, `100B.mat`, `101B.mat` depending on the 
+> orientation and whether they are amplitude or phase data
+
+
---
+++ Reading files/read_files.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 21 11:47:33 2024
+
+@author: pteysseyre
+"""
+
+from pymatreader import read_mat # To read the .mat files
+import os.path # Useful to determine if a file exist or not (to write headers in a new file)
+import glob
+import numpy as np
+
+#======================== Read mat files ======================================
+
+def read_Narrowband(la_date, la_station, path, Cal_NS = 1, Cal_EW = 1): 
+    """
+    Reads the data for the station and the day asked and stores them in the output arrays
+    NOTE: This code reads the latest created file (in case several files are present)
+
+    Inputs
+    ------------------
+    - la_date: Date, in the format 'yyyy_mm_dd'.
+    - la_station: Station call-sign (e.g. 'NSY')
+    - path: Path to the files (without their names, e.g. 'Users/example/Documents/FileFolder/')
+        NOTE: It is assumed that in FileFolder, files are organised by year, month and day:
+        e.g. FileFolder/2023/11/2023_11_05/
+        If this is not the case, please delete, change or comment the line below the comment "------- To change if needed -------"
+    - Cal_NS/Cal_EW: Calibration factor to apply on the NS and EW orientation (default: 1)
+
+    Outputs
+    -----------------
+    - amp_NS, amp_EW: Amplitude time series (in pT)
+    - phase_NS, phase_EW = Phase time series (in °)
+    - the_time: time-array
+
+    History
+    --------------------
+    Written by C. Briand on 16//10/2022
+    Modified and converted to Python on 18/03/2024"""
+    
+    the_year, the_month, the_day = la_date.split('_')
+    start_time = 0
+    
+    # -------------------- To change if needed -------------------------
+    path = path + str(the_year) + '/' + str(the_month) + '/' + la_date + '/'
+    # ------------------------------------------------------------------
+
+    # amp_NS
+    list_of_files = glob.glob(path+'*'+la_station+'_100A.mat')
+    try :
+        latest_file = max(list_of_files, key=os.path.getctime)
+    except ValueError: # If the path is wrong
+        print('Check the path to .mat files (read_Narrowband)')
+        amp_NS = np.nan
+        amp_EW = np.nan
+        phase_EW = np.nan
+        phase_NS = np.nan
+        the_time = np.nan
+        return (amp_NS, amp_EW, phase_NS, phase_EW, the_time)
+
+    try :
+        data = read_mat(latest_file)
+        amp_NS = data['data']*Cal_NS
+        start_time = data['start_hour'] + data['start_minute']/60 + data['start_second']/3600
+    except (TypeError, ValueError):
+        print('TypeError - amp_NS')
+        amp_NS = np.nan
+
+    
+    # phase_EW
+    list_of_files = glob.glob(path+'NC*'+la_station+'_101B.mat')
+    latest_file = max(list_of_files, key=os.path.getctime)
+
+    try :
+        data = read_mat(latest_file)
+        phase_EW = data['data']
+        start_time = data['start_hour'] + data['start_minute']/60 + data['start_second']/3600
+    except (TypeError, ValueError):
+        print('TypeError - phase_EW')
+        phase_EW = np.nan
+    
+    
+    # amp_EW
+    list_of_files = glob.glob(path+'NC*'+la_station+'_101A.mat')
+    latest_file = max(list_of_files, key=os.path.getctime)
+    try :
+        data = read_mat(latest_file)
+        amp_EW = data['data']*Cal_EW
+        start_time = data['start_hour'] + data['start_minute']/60 + data['start_second']/3600
+    except (TypeError, ValueError):
+        print('TypeError - amp_EW')
+        amp_EW = np.nan
+
+    # phase_NS
+    list_of_files = glob.glob(path+'NC*'+la_station+'_100B.mat')
+    latest_file = max(list_of_files, key=os.path.getctime)
+    try :
+        data = read_mat(latest_file)
+        phase_NS = data['data']
+        start_time = data['start_hour'] + data['start_minute']/60 + data['start_second']/3600
+    except (TypeError, ValueError):
+        print('TypeError - phase_NS')
+        phase_NS = np.nan
+    
+    Fs = data['Fs']
+    tstep = 1/Fs/3600
+    data_length = np.max([np.size(phase_NS), np.size(phase_EW), np.size(amp_NS), np.size(amp_EW)])
+    the_time = np.linspace(start_time, tstep*data_length, data_length)
+    
+    return (amp_NS, amp_EW, phase_NS, phase_EW, the_time)
+
+
+
